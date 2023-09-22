4d4133cfc6c7cd7df6af6842d1d0ba82d69120ed
Khaled Zra <halo_434000@hotmail.com>
2023-09-22 14:56:43 +0200

    Added NativeBase and patch to support latest react version

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,20 @@
 import {Slot} from 'expo-router';
-import { StyleSheet } from 'react-native';
 import {SafeAreaProvider, SafeAreaView} from "react-native-safe-area-context";
 import {StatusBar} from "expo-status-bar";
+import {NativeBaseProvider} from "native-base";
+import React from "react";
 
 export default function Layout() {
   return (
-    <SafeAreaProvider>
-      <SafeAreaView style={{ flex: 1, backgroundColor: 'grey' }}>
-        <StatusBar style="auto" />
+    // <SafeAreaProvider>
+    //
+    // </SafeAreaProvider>
+
+    <SafeAreaView style={{ flex: 1, backgroundColor: 'grey' }}>
+      <StatusBar style="auto" />
+      <NativeBaseProvider>
         <Slot />
-      </SafeAreaView>
-    </SafeAreaProvider>
+      </NativeBaseProvider>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,12 @@
-import {Text, View} from 'react-native';
 import { Link } from 'expo-router';
+import React from "react";
+import { Box } from "native-base";
 
-export default function Page() {
+export default function App() {
   return (
-    <View>
+    <Box>
       <Link href="/Berserk">Berserk</Link>
       <Link href="/mangaTopTen">Manga Top Ten</Link>
-    </View>
+    </Box>
   );
 }
