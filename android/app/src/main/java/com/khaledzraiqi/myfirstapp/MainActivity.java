624cac7235a5a4e87a44f54cef43d897dd133546
Khaled Zra <halo_434000@hotmail.com>
2023-11-17 15:17:27 +0100

    Changed project name

--- android/app/src/main/java/com/khaledzraiqi/myfirstapp/MainActivity.java
+++ android/app/src/main/java/com/khaledzraiqi/myfirstapp/MainActivity.java
@@ -1,4 +1,4 @@
-package com.khaledzraiqi.myfirstapp;
+package com.khaledzraiqi.mangawiki;
 
 import android.os.Build;
 import android.os.Bundle;
--- android/app/src/main/java/com/khaledzraiqi/myfirstapp/MainApplication.java
+++ android/app/src/main/java/com/khaledzraiqi/myfirstapp/MainApplication.java
@@ -1,4 +1,4 @@
-package com.khaledzraiqi.myfirstapp;
+package com.khaledzraiqi.mangawiki;
 
 import android.app.Application;
 import android.content.res.Configuration;
